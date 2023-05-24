37a6c7eb172eb7f7d7a08efc0c5a8748e48eedd6
ooogreen <128555417+ooogreen@users.noreply.github.com>
2023-05-24 00:23:58 +0000

    5/24 전송!

---
+++ elseif.c
@@ -0,0 +1,25 @@
+// #include <stdio.h>
+
+// main(){
+//   int x;
+//   printf("1:사과\n2:키위\n3:복숭아\n4:수박");
+//   scanf("%d", &x);
+  
+//   if(x==1)
+//   printf("사과를 좋아하는군요!");
+
+// else if (x==2)
+//   printf("키위를 좋아하는군요!");
+
+// else if (x==3)
+//   printf("복숭아를 좋아하는군요!");
+
+
+// else if (x==4)
+//   printf("수박을 좋아하는군요!");
+
+// else
+//   printf("당신은 아마도 과일을 좋아하지 않는 것 같군요!");
+
+// printf("좋아하는 과일 조사 끝");
+// }
